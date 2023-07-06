fb977f17e045dbc72d13ecc25a45823c5fbda3b8
Titus <titus@ubuntu23.04>
2023-07-06 12:08:30 +0300

    updated readme and js file

---
+++ README.md
@@ -0,0 +1,54 @@
+# Movie Theater System
+
+## Description
+
+The Movie Theater System is a web-based application built using HTML, CSS, and JavaScript. It provides users with a platform to view available movies, book tickets, and manage movie listings in a theater. The system interacts with a JSON server for storing movie data and ticket information.
+
+![Movie Theater System](resources/Screenshot%20from%202023-07-06%2011-47-30.png "Image of the full the full zoomed out page on opening the web-app")
+
+## Features
+
+- Display a menu of all movies in the theater
+- Show movie details including title, runtime, showtime, and available tickets
+- Buy tickets for a movie (if tickets are available)
+- Update ticket availability when tickets are purchased
+- Delete movies from the system
+- Display sold-out movies in the menu
+- Responsive design for mobile and desktop devices
+
+## Screenshots
+
+Images of the full zoomed out page and what is displayed when tickets are sold out and what is displayed when they are still available
+![display when tickets are sold out](resources/Screenshot%20from%202023-07-06%2011-55-49.png "display when tickets are sold out")
+![display when tickets are still available](resources/Screenshot%20from%202023-07-06%2011-56-07.png "display when tickets are still available")
+
+## Installation
+
+1. Clone the repository: `git clone https://github.com/muiatitus/movietheatre-code-challenge.git`
+2. Navigate to the project directory: `cd movietheatre-code-challenge`
+3. Start a local server (e.g., using VS Code Live Server extension or other server of your choice)
+
+## Usage
+
+1. Open the application in your web browser.
+2. Browse the menu of available movies on the left side of the page.
+3. Click on a movie to view its details on the right side of the page.
+4. If tickets are available, click the "Buy Ticket" button to purchase a ticket.
+5. The number of available tickets will update accordingly.
+6. To delete a movie, click the delete button next to the movie in the menu.
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+- JSON Server
+
+## Acknowledgements
+
+- The movie data used in this project is fictional and for demonstration purposes only.
+
+## License
+
+This project is licensed under the [MIT License](https://github.com/muiatitus/movietheatre-code-challenge/blob/main/LICENSE).
+
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Define API endpoints
 const filmEndpoint = "http://localhost:3000/films";
 const filmDetailsEndpoint = "http://localhost:3000/films/1";
+const filmsList = document.getElementById("films");
 
 // Define DOM elements
 const poster = document.getElementById("poster");
@@ -8,19 +9,13 @@ const title = document.getElementById("title");
 const runtime = document.getElementById("runtime");
 const showtime = document.getElementById("showtime");
 const availableTickets = document.getElementById("available-tickets");
-const filmsList = document.getElementById("films");
+const buyTicketButton = document.getElementById("buy-ticket");
 
-// Fetch film details and populate movie details section
+// Fetch film details for 'The Giant Gila Monster' and populate movie details section
 fetch(filmDetailsEndpoint)
   .then((response) => response.json())
   .then((data) => {
-    poster.src = data.poster;
-    title.textContent = data.title;
-    runtime.textContent = `Runtime: ${data.runtime} minutes`;
-    showtime.textContent = `Showtime: ${data.showtime}`;
-    availableTickets.textContent = `Available Tickets: ${
-      data.capacity - data.tickets_sold
-    }`;
+    displayMovieDetails(data);
   });
 
 // Fetch all films and populate films list
@@ -31,6 +26,74 @@ fetch(filmEndpoint)
       const li = document.createElement("li");
       li.textContent = film.title;
       li.classList.add("film", "item");
+      li.addEventListener("click", () => {
+        displayMovieDetails(film);
+      });
       filmsList.appendChild(li);
     });
   });
+
+// Display movie details
+function displayMovieDetails(film) {
+  poster.src = film.poster;
+  title.textContent = film.title;
+  runtime.textContent = `Runtime: ${film.runtime} minutes`;
+  showtime.textContent = `Showtime: ${film.showtime}`;
+  availableTickets.textContent = `Available Tickets: ${
+    film.capacity - film.tickets_sold
+  }`;
+
+  if (film.tickets_sold === film.capacity) {
+    buyTicketButton.disabled = true;
+    buyTicketButton.textContent = "Sold Out";
+  } else {
+    buyTicketButton.disabled = false;
+    buyTicketButton.textContent = "Buy Ticket";
+  }
+
+  buyTicketButton.addEventListener("click", () => {
+    if (film.tickets_sold < film.capacity) {
+      film.tickets_sold++;
+      availableTickets.textContent = `Available Tickets: ${
+        film.capacity - film.tickets_sold
+      }`;
+      updateTicketsSold(film.id, film.tickets_sold);
+    }
+  });
+}
+
+// Update tickets_sold on the server
+function updateTicketsSold(filmId, ticketsSold) {
+  fetch(`${filmEndpoint}/${filmId}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      tickets_sold: ticketsSold,
+    }),
+  })
+    .then((response) => response.json())
+    .then((updatedFilm) => {
+      availableTickets.textContent = `Available Tickets: ${
+        updatedFilm.capacity - updatedFilm.tickets_sold
+      }`;
+      if (updatedFilm.tickets_sold === updatedFilm.capacity) {
+        buyTicketButton.disabled = true;
+        buyTicketButton.textContent = "Sold Out";
+      }
+    });
+}
+
+// Delete a film from the server
+function deleteFilm(filmId) {
+  fetch(`${filmEndpoint}/${filmId}`, {
+    method: "DELETE",
+  })
+    .then(() => {
+      const filmItem = filmsList.querySelector(`li[data-id="${filmId}"]`);
+      if (filmItem) {
+        filmItem.remove();
+      }
+    });
+}
